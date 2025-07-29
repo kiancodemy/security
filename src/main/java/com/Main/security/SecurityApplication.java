928acefd769f7fb7c96806425e3a7ab9235f0beb
kianoosh <kinkheirani@gmail.com>
2025-07-28 23:50:13 -0800

    start

---
+++ src/main/java/com/Main/security/SecurityApplication.java
@@ -0,0 +1,21 @@
+package com.Main.security;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.print("it is kian");
+		};
+	}
+
+}
